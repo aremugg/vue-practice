44bf5cddeb793794611c0f0a93dc4a98a3322109
aremugg <77441476+aremugg@users.noreply.github.com>
2023-03-15 12:38:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-practice
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-wgqg59)
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <p> {{house}}</p>
+    <button @click="trenches" style="background-color: red"> MAINLAND </button>
+    <button @click="manipulate" style="background-color: blue;"> ISLAND </button>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      house: "iyana Ipaja"
+    }
+  },
+  methods:{
+    manipulate(){
+      this.house = "lekki phase one"
+    },
+    trenches(){
+      this.house = "Akute Ogun STATE"
+    }
+
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
